e2bb8229aa0b2979c5607e39cb227f1e8e8cf0a8
JYOTI <jyotiprakashkhuntia999@gmail.com>
2026-06-19 20:33:01 +0530

    Fix Flask syntax

--- app/app.py
+++ app/app.py
@@ -1,63 +1,52 @@
 from flask import Flask, request, jsonify
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 orders = {}
 
 @app.route("/")
 def home():
-return {
-"message": "Cloud Native Order Tracking Platform Running on EKS"
-}
+    return {
+        "message": "Cloud Native Order Tracking Platform Running on EKS"
+    }
 
 @app.route("/health")
 def health():
-return {
-"status": "healthy"
-}
+    return {
+        "status": "healthy"
+    }
 
 @app.route("/orders", methods=["POST"])
 def create_order():
-data = request.json
+    data = request.json
+    order_id = str(len(orders) + 1)
 
-```
-order_id = str(len(orders) + 1)
+    orders[order_id] = {
+        "order_id": order_id,
+        "customer": data.get("customer"),
+        "product": data.get("product"),
+        "status": "Created"
+    }
 
-orders[order_id] = {
-    "order_id": order_id,
-    "customer": data.get("customer"),
-    "product": data.get("product"),
-    "status": "Created"
-}
-
-return jsonify(orders[order_id]), 201
-```
+    return jsonify(orders[order_id]), 201
 
 @app.route("/orders", methods=["GET"])
 def get_orders():
-return jsonify(list(orders.values()))
+    return jsonify(list(orders.values()))
 
 @app.route("/orders/<order_id>", methods=["GET"])
 def get_order(order_id):
-if order_id not in orders:
-return jsonify({"error": "Order not found"}), 404
-
-```
-return jsonify(orders[order_id])
-```
+    if order_id not in orders:
+        return jsonify({"error": "Order not found"}), 404
+    return jsonify(orders[order_id])
 
 @app.route("/orders/<order_id>/status", methods=["PUT"])
 def update_status(order_id):
-if order_id not in orders:
-return jsonify({"error": "Order not found"}), 404
+    if order_id not in orders:
+        return jsonify({"error": "Order not found"}), 404
+    data = request.json
+    orders[order_id]["status"] = data.get("status")
+    return jsonify(orders[order_id])
 
-```
-data = request.json
-
-orders[order_id]["status"] = data.get("status")
-
-return jsonify(orders[order_id])
-```
-
-if **name** == "**main**":
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
